b00548015a43ead07b3ee2303ae461bef7b0d697
ey-01 <eyamazaki@vmware.com>
2023-01-28 23:56:20 +0900

    fix: update stage8_play/pause

--- stage8.js
+++ stage8.js
@@ -1,20 +1,16 @@
 let bgm = new Audio(
-<<<<<<< HEAD
   "https://drive.google.com/uc?id=1oiDiUZIGqQVO4YAuzZzSYcTwjOTVJW9L"
 );
 
 bgm.volume = 0.1;
 
-document.getElementById('bgm-play').addEventListener('click', () => {
+/*document.getElementById('bgm-play').addEventListener('click', () => {
     bgm.play();
-  });
-=======
-    "https://drive.google.com/uc?id=1oiDiUZIGqQVO4YAuzZzSYcTwjOTVJW9L"
-  );
-  
-  bgm.volume = 0.1;
-  
-  document.getElementById('bgm-play').addEventListener('click', () => {
-      bgm.play();
-    });
->>>>>>> 5bc94ebccea3cb93e1f2707fc23b747d25d56a48
+});*/
+
+
+let myAudio = document.getElementById("myAudio");
+
+function togglePlay() {
+  return myAudio.paused ? myAudio.play() : myAudio.pause();
+};
