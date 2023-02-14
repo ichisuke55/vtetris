59861baecdf90be5da645d31757924805b9683c2
ey-01 <eyamazaki@vmware.com>
2023-02-14 19:59:40 +0900

    fix: complete stage8

--- stage8.js
+++ stage8.js
@@ -1,9 +1,17 @@
 let bgm = new Audio(
-    "https://drive.google.com/uc?id=1oiDiUZIGqQVO4YAuzZzSYcTwjOTVJW9L"
-  );
-  
-  bgm.volume = 0.1;
-  
-  document.getElementById('bgm-play').addEventListener('click', () => {
-      bgm.play();
-    });
+  "https://drive.google.com/uc?id=1oiDiUZIGqQVO4YAuzZzSYcTwjOTVJW9L"
+);
+
+bgm.volume = 0.1;
+
+let playing = false;
+
+document.getElementById("bgm-play").addEventListener("click", () => {
+  if (playing === false) {
+    playing = true;
+    bgm.play();
+  } else {
+    playing = false;
+    bgm.pause();
+  }
+});
