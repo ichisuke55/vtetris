151a4b3c1d48131ba72005a79d358aef94201722
ichisuke55 <campistatrequartista10@gmail.com>
2023-03-06 17:25:36 +0900

    fix: stage8 for question

--- stage8.js
+++ stage8.js
@@ -1,17 +1,24 @@
 let bgm = new Audio(
   "https://drive.google.com/uc?id=1oiDiUZIGqQVO4YAuzZzSYcTwjOTVJW9L"
 );
+// ファイル設置場所: https://drive.google.com/drive/u/0/folders/1pWyJzsvz0xxmq7DZJvpARj5ALDhgacl2
 
 bgm.volume = 0.1;
 
 let playing = false;
 
-document.getElementById("bgm-play").addEventListener("click", () => {
+document.getElementById("").addEventListener("", () => {
   if (playing === false) {
-    playing = true;
-    bgm.play();
+    // playing フラグを書きかえるよ
+
+    // 音楽を流すよ
+
+    document.getElementById("bgm-play").innerHTML = "Stop";
   } else {
-    playing = false;
-    bgm.pause();
+    // playing フラグを書きかえるよ
+
+    // 音楽を止めるよ
+
+    document.getElementById("bgm-play").innerHTML = "Start";
   }
 });
