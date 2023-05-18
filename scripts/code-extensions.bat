10fc5f313ba7e041f6e8e74a642148c263684780
Seishi Murakami <smurakami@smurakami0K4YL.vmware.com>
2023-05-18 09:44:46 +0900

    add code-extensions.bat for Windows env

---
+++ scripts/code-extensions.bat
@@ -0,0 +1,12 @@
+code --force --install-extension bpruitt-goddard.mermaid-markdown-syntax-highlighting ^
+& code --force --install-extension ecmel.vscode-html-css ^
+& code --force --install-extension esbenp.prettier-vscode ^
+& code --force --install-extension george-alisson.html-preview-vscode ^
+& code --force --install-extension mohd-akram.vscode-html-format ^
+& code --force --install-extension mohsen1.prettify-json ^
+& code --force --install-extension PKief.material-icon-theme ^
+& code --force --install-extension quicktype.quicktype ^
+& code --force --install-extension vscode-icons-team.vscode-icons ^
+& code --force --install-extension ZainChen.json
+
+pause
